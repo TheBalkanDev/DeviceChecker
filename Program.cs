2354546ddad788c665d58d978cfe400fdf0ced9d
BalkanDev <75144786+TheBalkanDev@users.noreply.github.com>
2021-10-30 17:12:31 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,24 +7,10 @@ public class Program
         Console.WriteLine("Welches iPhone wollen Sie verkaufen?");
         string iphone = Console.ReadLine();
         
-        Console.WriteLine("Wie vieli Speicherplatz hat das iPhone?");
+        Console.WriteLine("Wie viel Speicherplatz hat das iPhone?");
         string speicher = Console.ReadLine();
         
         Console.WriteLine("Ihr iPhone ist das " + iphone + " und es hat " + speicher + " Gigabyte");
-        
-        switch (iphone)
-        {
-         case iphone5s:
-         Console.WriteLine("iPhone 5s");
-                break;
-                
-          case iphone7:
-                Console.WriteLine("iPhone 7!");
-                break;
-                
-            default:
-            Console.WriteLine("Error, Unbekanntes iPhone");
-                break;
         }
     }
 }
