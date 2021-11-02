0664f371657880d031632cc1f48cda36a48228f0
BalkanDev <75144786+TheBalkanDev@users.noreply.github.com>
2021-11-03 00:05:33 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,13 +4,54 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Welches iPhone wollen Sie verkaufen?");
-        string iphone = Console.ReadLine();
+        Console.WriteLine("Welches Smartphone wollen Sie verkaufen?");
+        string handy = Console.ReadLine();
         
-        Console.WriteLine("Wie viel Speicherplatz hat das iPhone?");
-        string speicher = Console.ReadLine();
+        switch(handy)
+        {
+            case "iPhone4s":
+             Console.WriteLine("iPhone 4s!");
+                break;
+                
+            case "iPhone6s":
+                Console.WriteLine("iPhone 6s!");
+                break;
+                
+            case "SamsungS4":
+                Console.WriteLine("Samsung S4!");
+                break;
+                
+             case "SamsungS6":
+                Console.WriteLine("Samsung S6!");
+                break;
+                
+            default:
+                Console.WriteLine("Syntax Error");
+                break;
+        }
         
-        Console.WriteLine("Ihr iPhone ist das " + iphone + " und es hat " + speicher + " Gigabyte");
+        Console.WriteLine("Wie viel Speicherplatz hat das Gerät?");
+        int speicher = Console.ReadLine();
+        
+        switch(speicher)
+        {
+        case 16:
+           Console.WriteLine("16 GB");
+           break;
+              
+         case 32:
+             Console.WriteLine("32 GB");
+                break;
+                
+          case 64
+             Console.WriteLine("64 GB");
+             break;
+                
+           case 128:
+               Console.WriteLine("128 GB");
+               break;
+ 
+        Console.WriteLine("Analyse: Ihr iPhone ist das " + iphone + " und es hat " + speicher + " Gigabyte");
         }
     }
 }
