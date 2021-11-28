24a6603f92777545a62470fc4cf2556c5bd5ad94
BalkanDev <75144786+TheBalkanDev@users.noreply.github.com>
2021-11-28 21:36:03 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -68,50 +68,68 @@ public class Program
         }
         
         Console.WriteLine("Wie viel Speicherplatz hat das Gerät?");
-        int speicher = Console.ReadLine();
-        double zwischenergebniss;
+         string speicher = Console.ReadLine();
         
         switch(speicher)
         {
-        case 16:
-           Console.WriteLine("16 GB");
-           zwischenergebniss = wert;
-           break;
+        case "16":
+        case "16GB":
+        case "16gb":
+        case "16 GB":
+        case "16 gb":
+                
+       Console.WriteLine("Ihr Speicher: 16 GB");
+       break;
               
-         case 32:
-             Console.WriteLine("32 GB");
-             zwischenergebniss = wert;
-             break;
+        case "32":
+        case "32GB":
+        case "32gb":
+        case "32 GB":
+        case "32 gb":
                 
-            case 64:
-             Console.WriteLine("64 GB");
-             zwischenergebniss = wert + 5;
-             break;
+        Console.WriteLine("Ihr Speicher: 32 GB");
+        break;
                 
-           case 128:
-               Console.WriteLine("128 GB");
-               zwischenergebniss = wert + 10;
-               break;
+        case "64":
+        case "64GB":
+        case "64gb":
+        case "64 GB":
+        case "64 gb":
                 
-            default:
-                Console.WriteLine("Error";
-                break;
-        }
+        Console.WriteLine("Ihr Speicher: 64 GB");
+        wert = wert + 5;
+        break;
+                
+        case "128":
+        case "128GB":
+        case "128gb":
+        case "128 GB":
+        case "128 gb":
+                
+        Console.WriteLine("Ihr Speicher: 128 GB");
+        wert = wert + 10;
+        break;
+                
+       default:
+       Console.WriteLine("Error";
+       break;         
+     }
                                   
      Console.WriteLine("Lässt sich das Gerät einwandfrei nutzen?");
-     string funktion = Console.ReadLine();
-     bool funktion;
+     string funktion = Console.ReadLine():
                                   
        switch(funktion)
        {    
            case "Ja":
-           Console.WtiteLine("Ja");
-           funktion2 = true;
+           case "ja":
+           
+           Console.WtiteLine("Alles klar");
            break;
            
-           case "Nein";
-           Console.WriteLine("Nein");
-           funktion2 = false;
+           case "Nein":
+           case "nein":
+           
+           Console.WriteLine("Alles klar");
            break;
            
          Console.WriteLine("Sind Kratzer/Gebrauchsspuren am Gerät oder am Display zu erkennen?");
@@ -120,10 +138,12 @@ public class Program
           switch(kratzer)
           {
               case "Viele":
-               
-         // Ende
+              case "viele":
+                  
+              Console.WriteLine();
+              break;
+          }
+           
         Console.WriteLine("Analyse: Ihr Smartphone ist das " + iphone + " und es hat " + speicher + " Gigabyte. Der wert beträgt" + zwischenergebnis + " Euro!");
           }
         }
-     }
-  }
